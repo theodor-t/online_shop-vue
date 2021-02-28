cf6adb532015520a620391bd01106780ab2695d7
theodorerlax <sampjohn@mail.ru>
2021-02-28 22:41:54 +0200

    corrections

--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -3,26 +3,16 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
-
-<!--    <v-catalog/>-->
-<!--    <v-cart
-        v-if="CART.length"
-        :cart_data="CART"
-    />-->
   </div>
 
 </template>
 
 <script>
-/*  import vCatalog from './v-catalog'*/
-/*  import vCart from './v-cart'*/
   import {mapGetters} from 'vuex'
 
   export default {
     name:"v-main-wrapper",
     components: {
-/*      vCatalog,*/
-/*      vCart*/
     },
     props: {},
     data(){
