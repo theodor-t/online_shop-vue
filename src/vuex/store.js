af00e9d9cc1621435c37dc5ee0a6e3146218c203
theodorerlax <sampjohn@mail.ru>
2021-02-28 19:20:51 +0200

    create store.js

---
+++ src/vuex/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+let store = new Vuex.store({
+    state: {},
+    mutations: {},
+    actions: {},
+    getters:{}
+});
+
+export default store;
