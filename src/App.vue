425f3ac6b9b02648a5ad3d4a7ec213cc46b89dcf
theodorerlax <sampjohn@mail.ru>
2021-02-28 14:56:01 +0200

    add components for site

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
   <p>Hello World</p>
+    <v-main-wrapper>
+
+    </v-main-wrapper>
   </div>
 </template>
 
 <script>
-
+import vMainWrapper from './components/v-main-wrapper'
 
 export default {
   name: 'App',
+  components:{
+    vMainWrapper
+  }
 }
 </script>
 
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="v-main-wrapper">
+    <p>{{title}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"v-main-wrapper",
+    components: {},
+    props: {},
+    data(){
+      return {
+        title: 'Main wrapper'
+      }
+    },
+    computed:{},
+    methods: {},
+    watch: {},
+    mounted() {
+      console.log('Darova')
+    }
+  }
+</script>
+
+<style>
+.v-main-wrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
