3957fd1119aeacb176689919feb3a3c3e2216fd1
theodorerlax <sampjohn@mail.ru>
2021-02-28 21:20:51 +0200

    add quantity to cart

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -15,7 +15,16 @@ let store = new Vuex.Store({
         },
         SET_CART: (state, product) => {
             if (state.cart.length) {
-
+                let isProductExists = false;
+                state.cart.map(function (item){
+                    if (item.article === product.article){
+                        isProductExists = true;
+                        item.quantity++;
+                    }
+                })
+                if (!isProductExists){
+                    state.cart.push(product)
+                }
             } else {
                 state.cart.push(product)
             }
