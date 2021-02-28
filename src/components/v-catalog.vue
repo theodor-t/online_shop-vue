d135dc242d8ed734f8f28c0a4466a5329629df28
theodorerlax <sampjohn@mail.ru>
2021-02-28 19:58:41 +0200

    connected to json db

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
     <h1>Catalog</h1>
     <div class="v-catalog__list">
       <v-catalog-item
-          v-for="product in products"
+          v-for="product in PRODUCTS"
           :key="product.article"
           :product_data="product"
           @sendDataToParent="showChildArticleInConsole"
@@ -14,6 +14,8 @@
 
 <script>
 import vCatalogItem from './v-catalog-item'
+import {mapActions, mapGetters} from 'vuex'
+
 export default {
   name: "v-catalog",
   components: {
@@ -23,43 +25,28 @@ export default {
   data() {
     return {
 
-      products: [
-      {
-        image: "1.jpg",
-        name: "T-shirt 1",
-        price: 2100.234234234,
-        article: "T1",
-        available: true,
-        category: "Мужские"
-      },
-        {
-          image: "2.jpg",
-          name: "T-shirt 2",
-          price: 2100.234234234,
-          article: "T2",
-          available: true,
-          category: "Мужские"
-        },
-        {
-          image: "3.jpg",
-          name: "T-shirt 3",
-          price: 2100.234234234,
-          article: "T3",
-          available: true,
-          category: "Мужские"
-        }
-
-    ]
     }
   },
-  computed: {},
+  computed: {
+    ...mapGetters([
+        'PRODUCTS'
+    ]),
+  },
   methods:{
+    ...mapActions([
+        'GET_PRODUCTS_FROM_API'
+    ]),
     showChildArticleInConsole(data) {
       console.log(data)
     }
   },
   mounted() {
-    console.log('test')
+    this.GET_PRODUCTS_FROM_API()
+    .then((response) => {
+      if(response.data){
+        console.log('Data arrived!')
+      }
+    })
   }
 }
 </script>
